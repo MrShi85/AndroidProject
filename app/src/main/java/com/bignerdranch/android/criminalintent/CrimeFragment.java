12d01a415d3066a34fd64443c9a84296930d29e1
Sergey Shilkin <ioda85@mail.ru>
2016-08-13 00:14:11 +0300

    chapter 14_1 database

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -130,4 +130,12 @@ public class CrimeFragment extends Fragment {
             updateDate();
         }
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        CrimeLab.get(getActivity())
+                .updateCrime(mCrime);
+    }
 }
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -1,9 +1,13 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.bignerdranch.android.criminalintent.database.CrimeBaseHelper;
+import com.bignerdranch.android.criminalintent.database.CrimeCursorWrapper;
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +20,6 @@ public class CrimeLab {
 
     private static CrimeLab sCrimeLab;
 
-    private List<Crime> mCrimes;
     private Context mContext;
     private SQLiteDatabase mDatabase;
 
@@ -28,42 +31,89 @@ public class CrimeLab {
     }
 
     public void addCrime(Crime c){
-        mCrimes.add(c);
+        ContentValues values = getContentValues(c);
+
+        mDatabase.insert(CrimeTable.NAME,null, values);
     }
 
     private CrimeLab(Context context){
         mContext = context.getApplicationContext();
         mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
-        mCrimes = new ArrayList<>();
-        /*for (int i = 0; i<100; i++){
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i%2==0);
-            mCrimes.add(crime);
-        }*/
     }
 
-    public List<Crime> getCrimes(){
-        return mCrimes;
+    public List<Crime> getCrimes()
+    {
+        List<Crime> crimes = new ArrayList<>();
+
+        CrimeCursorWrapper cursor = queryCrimes(null,null);
+
+        try{
+            cursor.moveToFirst();
+            while(!cursor.isAfterLast()){
+                crimes.add(cursor.getCrime());
+                cursor.moveToNext();
+            }
+        }
+        finally {
+            cursor.close();
+        }
+        return crimes;
     }
 
     public Crime getCrime(UUID id){
-        for (Crime crime : mCrimes){
-            if(crime.getId().equals(id)){
-                return crime;
+
+        CrimeCursorWrapper cursor = queryCrimes(
+                CrimeTable.Cols.UUID + " = ?",
+                new String[] {id.toString()});
+        try{
+            if(cursor.getCount()==0){
+                return null;
             }
+        }
+        finally {
+            cursor.close();
         }
         return null;
     }
 
+    public void updateCrime(Crime crime){
+        String uuidString = crime.getId().toString();
+        ContentValues values = getContentValues(crime);
+
+        mDatabase.update(CrimeTable.NAME, values,
+                CrimeTable.Cols.UUID + " = ?" ,
+                new String[]{uuidString});
+    }
+
+    private static ContentValues getContentValues(Crime crime){
+        ContentValues values = new ContentValues();
+        values.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved()? 1 : 0);
+
+        return values;
+    }
+
+    private CrimeCursorWrapper queryCrimes(String whereClause, String[] whereArgs){
+        Cursor cursor = mDatabase.query(
+                CrimeTable.NAME,
+                null, // Columns-null выбирает все столбцы
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null);
+        return  new CrimeCursorWrapper(cursor);
+    }
     public int getPosition(UUID id){
-        int i=0;
+        /*int i=0;
         for (Crime crime : mCrimes){
             if(crime.getId().equals(id)){
                 return i;
             }
             i++;
-        }
+        }*/
         return -1;
     }
 }
